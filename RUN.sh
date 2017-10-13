e542aea4c04c8c43acb17705c960c9443333bf90
Rajiv V Mantena <mantenarajiv@gmail.com>
2017-10-12 22:01:37 -0600

    Add files via upload

---
+++ RUN.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sshpass -p 'Vijayalakshmi9$' scp ./* mantena@lab2-19.eng.utah.edu:/home/mantena/public_html/new/
+sshpass -p 'Vijayalakshmi9$' scp ./style/* mantena@lab2-19.eng.utah.edu:/home/mantena/public_html/new/style/
+sshpass -p 'Vijayalakshmi9$' scp ./images/* mantena@lab2-19.eng.utah.edu:/home/mantena/public_html/new/images/
