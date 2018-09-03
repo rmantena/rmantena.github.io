3bd1b3a76d3902e6e389c8f9419c3525a4366ab8
Rajiv Mantena <mantenarajiv@gmail.com>
2018-09-03 03:22:56 -0600

    Deleting to safeguard credentials

--- RUN.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sshpass -p 'Vijayalakshmi9$' scp ./* mantena@lab2-19.eng.utah.edu:/home/mantena/public_html/new/
-sshpass -p 'Vijayalakshmi9$' scp ./style/* mantena@lab2-19.eng.utah.edu:/home/mantena/public_html/new/style/
-sshpass -p 'Vijayalakshmi9$' scp ./images/* mantena@lab2-19.eng.utah.edu:/home/mantena/public_html/new/images/
